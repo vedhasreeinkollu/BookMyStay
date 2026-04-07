1cfd8f1fcdfafc0e234308ac9a977a7121280b97
vedhasreeinkollu <vedhasreeinkollu18@gmail.com>
2026-04-07 14:15:14 +0530

    Use Case 8: Booking History & Reporting

---
+++ BookMyStay/src/UseCase8BookingHistoryReporting.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UseCase8BookingHistoryReporting {
+
+    static class Reservation {
+        private String guestName;
+        private String roomType;
+
+        public Reservation(String guestName, String roomType) {
+            this.guestName = guestName;
+            this.roomType = roomType;
+        }
+
+        public String getGuestName() {
+            return guestName;
+        }
+
+        public String getRoomType() {
+            return roomType;
+        }
+    }
+
+    static class BookingHistory {
+        private List<Reservation> confirmedReservations;
+
+        public BookingHistory() {
+            confirmedReservations = new ArrayList<>();
+        }
+
+        public void addReservation(Reservation reservation) {
+            confirmedReservations.add(reservation);
+        }
+
+        public List<Reservation> getConfirmedReservations() {
+            return confirmedReservations;
+        }
+    }
+
+    static class BookingReportService {
+        public void generateReport(BookingHistory history) {
+            System.out.println("Booking History Report");
+            for (Reservation r : history.getConfirmedReservations()) {
+                System.out.println("Guest: " + r.getGuestName() + ", Room Type: " + r.getRoomType());
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        BookingHistory history = new BookingHistory();
+
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+
+        BookingReportService reportService = new BookingReportService();
+        reportService.generateReport(history);
+    }
+}
